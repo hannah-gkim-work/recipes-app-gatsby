dc0b67b7858c9c1248b244ee0d813fbe5a9c2c4d
Hannah Gomez Kim <hannah.gomezkim@slalom.com>
2022-01-25 11:45:34 -0600

    working on the navbar, logo added so far

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,38 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FiAlignJustify } from "react-icons/fi"
+import logo from "../assets/images/logo.svg"
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="/recipes">recipes </Link>
-        </li>
-        <li>
-          <Link to="/tags">tags </Link>
-        </li>
-        <li>
-          <Link to="/about">about </Link>
-        </li>
-        <li>
-          <Link to="/contact">contact </Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="nav-links show-links"></div>
+      </div>
     </nav>
+    // <nav>
+    //   <FiAlignJustify />
+    //   <ul>
+    //     <li>
+    //       <Link to="/">Home</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/recipes">Recipes</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/tags">Tags</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/about">About</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="/contact">Contact</Link>
+    //     </li>
+    //   </ul>
+    // </nav>
   )
 }
